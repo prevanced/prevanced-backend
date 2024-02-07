6e51bd442843186602adc263c630beb6e16d1644
Mohammed Rabil <rabil@techie.com>
2024-02-07 23:15:19 +0530

    Add Rocket.toml and update Spacefile and main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,5 @@
-#[macro_use] extern crate rocket;
-use rocket::figment::{providers::{Env, Format, Serialized, Toml}, Figment, Profile};
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Deserialize, Serialize)]
-struct Config {
-    port: u16,
-    /* and so on.. */
-}
-
-impl Default for Config {
-    fn default() -> Config {
-        let port_string: String = std::env::var("PORT").unwrap_or("8000".to_string());
-        let port: u16 = port_string.parse().unwrap();
-        Config {
-            port,
-        }
-    }
-}
-
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -26,10 +8,5 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    let figment = Figment::from(rocket::Config::default())
-        .merge(Serialized::defaults(Config::default()))
-        .merge(Toml::file("App.toml").nested())
-        .merge(Env::prefixed("APP_").global())
-        .select(Profile::from_env_or("APP_PROFILE", "default"));
-    rocket::custom(figment).mount("/", routes![index])
+    rocket::build().mount("/", routes![index])
 }
