59c5f78a6dcac1e8f1924605d651f5adf0b11b20
Mohammed Rabil <rabil@techie.com>
2024-02-08 11:16:12 +0530

    Add key-based authentication middleware

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 use axum::{
-    routing::{get, post},
+    middleware::from_fn,
+    routing::{delete, get, post},
     Router,
 };
 mod handlers;
+mod middleware;
 mod types;
 
 use handlers::*;
+use middleware::key_auth;
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
     let app = Router::new()
         .route("/", get(root))
+        .route("/", post(register_device).layer(from_fn(key_auth)))
+        .route("/", delete(delete_device).layer(from_fn(key_auth)))
         .route("/ping", get(ping))
-        .route("/register", post(register_device))
-        .route("/delete", post(delete_device))
-        .route("/send", post(send_notification));
+        .route("/send", post(send_notification).layer(from_fn(key_auth)));
 
     let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
     let address: String = std::env::var("ADDRESS").unwrap_or_else(|_| "127.0.0.1".to_string());
---
+++ src/middleware.rs
@@ -0,0 +1,36 @@
+use axum::{
+    extract::Request,
+    http::{HeaderMap, StatusCode},
+    middleware::Next,
+    response::Response,
+};
+
+// Define your custom middleware for key-based authentication
+pub async fn key_auth(
+    // run the `HeaderMap` extractor
+    headers: HeaderMap,
+    // you can also add more extractors here but the last
+    // extractor must implement `FromRequest` which
+    // `Request` does
+    request: Request,
+    next: Next,
+) -> Result<Response, StatusCode> {
+    match get_token(&headers) {
+        Some(token) if token_is_valid(token) => {
+            let response = next.run(request).await;
+            Ok(response)
+        }
+        _ => Err(StatusCode::UNAUTHORIZED),
+    }
+}
+
+fn get_token(headers: &HeaderMap) -> Option<&str> {
+    headers
+        .get("Authorization")
+        .and_then(|value| value.to_str().ok())
+        .and_then(|value| value.strip_prefix("Bearer "))
+}
+
+fn token_is_valid(token: &str) -> bool {
+    token == "secret"
+}
